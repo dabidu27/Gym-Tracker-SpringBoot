b4a6cba85e36a5e2e4124ec93b37e25a95db70ef
david <adamita.david@gmail.com>
2026-03-08 22:35:09 +0200

    Added the User entity

--- src/main/java/com/gymtracker/demo/entity/User.java
+++ src/main/java/com/gymtracker/demo/entity/User.java
@@ -2,30 +2,41 @@ package com.gymtracker.demo.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
-//we use lombook annotations to avoid
+
+//we use lombook annotations to avoid writting constructors and getters and setter
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    @Column(unique = true)
+    //@NotNull (add Column(nullable = false) in the entity, and @NotNull will be in the DTOs
+    @Column(unique = true, nullable = false)
     private String username;
 
-    @NotNull
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
-    @NotNull
+    @Column(nullable = false)
     private String hashedPassword;
 
-    private Date created_at;
+    @Column(updatable = false) //once set, the DB will never update this column, because we will auto generate the values for this column
+    private LocalDateTime createdAt;
+
+    //createdAt automatic generation
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
 }
